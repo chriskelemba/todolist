4eb98c8aa272ac561e86354f710f6abae029676b
Chris <chris@gmail.com>
2024-02-22 01:33:27 +0300

    added few component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import GetDate from './components/Date';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <GetDate/>
+      <Form/>
     </div>
   );
 }
