582b243a45eb6255a2f5fadcbf61e0d22b94290d
chriskelemba <loverhxchris@gmail.com>
2024-02-26 19:43:58 +0300

    first stage of work working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import GetDate from './components/Date';
 import MyTask from './components/Home';
+import TaskDetails from './components/TaskDetails';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <GetDate/>
-      <MyTask/>
-    </div>
+    <Router>
+      <div className="App">
+        <GetDate/>
+        <div className="Content">
+          <Switch>
+            <Route exact path="/">
+              <MyTask/>
+            </Route>
+            <Route path="/Task-details/:id">
+              <TaskDetails/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
